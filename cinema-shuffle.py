2b9cc85ef748701b82b4843eecaacf6fe9d94a88
helloyashsharma <167448808+helloyashsharma@users.noreply.github.com>
2024-04-20 14:04:36 +0530

    added search by year range
    Users can now specify the year range to search for movies.

--- cinema-shuffle.py
+++ cinema-shuffle.py
@@ -3,13 +3,21 @@ import requests
 # Variable to store json response from api
 output = "string"
 
+# Boolean to check years
+isIYearOk = False
+isFYearOk = False
+
+# Variables to store year ranges
+intialyear = 1900
+finalyear = 2024
+
 # Ask user for tmdb bearer token
 bearer_token = input("Enter your bearer token to start using the app:")
 print("Token accepted.")
 
 # Function to send a request to the api
 def ping():
-    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=2&primary_release_date.gte="+intialyear+"&primary_release_date.lte="+finalyear+"&sort_by=popularity.desc"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer " + bearer_token
@@ -31,13 +39,40 @@ def parse():
         print(output['results'][i]['title'])
         i +=1
 
+# Function to format the input years from user
+def formatYear():
+    global intialyear
+    global finalyear
+
+    intialyear = str(intialyear)+"-01-01"
+    finalyear = str(finalyear)+"-12-31"
 
 # Provide user options
 option = input("\nWhat would you like to do ? \n\n1.Discover movies to watch.\n\nSelect from options: 1\n")
 
 # Check user input
 if option == "1":
+
+# Ask for year range
+    while isIYearOk == False:
+        intialyear = int(input("\nInput initial year for searching movies: \nE.g. 2004\n"))
+        if 1900 > intialyear or intialyear > 2024:
+            print("\nInvalid value, select a year between 1900 and 2024.\n")
+        else:
+            isIYearOk = True
+
+    while isFYearOk == False:
+        finalyear = int(input("\nInput final year for searching movies: \nE.g. 2010\n"))
+        if 1900 > finalyear or finalyear > 2024:
+            print("\nInvalid value, select a year between 1900 and 2024.\n")
+        elif finalyear < intialyear:
+            print("\nInvalid value, final year can't be smaller than initial year.\n")
+        else:
+            isFYearOk = True
+
+    formatYear()
     ping()
     parse()
+    
 else:
     print("Improper input")
