c83e242edd49d869e1999f2810873204dd148d97
helloyashsharma <167448808+helloyashsharma@users.noreply.github.com>
2024-04-21 17:40:21 +0530

    better input error handling, more movie info
    Handled non int inputs for year range. Results will now provide more info such as synopsis and rating.

--- cinema-shuffle.py
+++ cinema-shuffle.py
@@ -20,7 +20,7 @@ def ping():
     if option == "1":
         url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc"
     elif option == "2":
-        url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=2&primary_release_date.gte="+intialyear+"&primary_release_date.lte="+finalyear+"&sort_by=popularity.desc"
+        url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&primary_release_date.gte="+intialyear+"&primary_release_date.lte="+finalyear+"&sort_by=popularity.desc"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer " + bearer_token
@@ -39,7 +39,10 @@ def parse():
 
     i = 0
     while i < lengthOfResults:
-        print(output['results'][i]['title'])
+        print("Name: "+output['results'][i]['title'])
+        print("Released on: "+output['results'][i]['release_date'])
+        print("Rating: {:.2f}".format(output['results'][i]['vote_average'])) # Truncating rating up to 2 decimal places
+        print("Synopsis: "+output['results'][i]['overview']+"\n")
         i +=1
 
 # Function to format the input years from user
@@ -61,20 +64,26 @@ if option == "1":
 elif option == "2":
 # Ask for year range
     while isIYearOk == False:
-        intialyear = int(input("\nInput initial year for searching movies: \nE.g. 2004\n"))
-        if 1900 > intialyear or intialyear > 2024:
-            print("\nInvalid value, select a year between 1900 and 2024.\n")
-        else:
-            isIYearOk = True
+        try:
+            intialyear = int(input("\nInput initial year for searching movies: \nE.g. 2004\n"))
+            if 1900 > intialyear or intialyear > 2024:
+                print("\nInvalid value, select a year between 1900 and 2024.\n")
+            else:
+                isIYearOk = True
+        except ValueError:
+            print("\nYear should be a number.\n")
 
     while isFYearOk == False:
-        finalyear = int(input("\nInput final year for searching movies: \nE.g. 2010\n"))
-        if 1900 > finalyear or finalyear > 2024:
-            print("\nInvalid value, select a year between 1900 and 2024.\n")
-        elif finalyear < intialyear:
-            print("\nInvalid value, final year can't be smaller than initial year.\n")
-        else:
-            isFYearOk = True
+        try:
+            finalyear = int(input("\nInput final year for searching movies: \nE.g. 2010\n"))
+            if 1900 > finalyear or finalyear > 2024:
+                print("\nInvalid value, select a year between 1900 and 2024.\n")
+            elif finalyear < intialyear:
+                print("\nInvalid value, final year can't be smaller than initial year.\n")
+            else:
+                isFYearOk = True
+        except ValueError:
+            print("\nYear should be a number.\n")
 
     formatYear()
     ping()
