11c803203cceebae60557948ca316181afd029ce
helloyashsharma <167448808+helloyashsharma@users.noreply.github.com>
2024-05-29 23:02:20 +0530

    added hide widget function
    The function can be used to hide widgets that are not needed on the current window.

--- cinema-shuffle.py
+++ cinema-shuffle.py
@@ -452,7 +452,11 @@ def GUI():
     frame2 = ctk.CTkScrollableFrame(master=window, width=600, height=500)
     cal = Calendar(master=frame2)
     cal2 = Calendar(master=frame2)
-    button2 = ctk.CTkButton(master=window, text="Discover Movies", font=('Merienda', 16), command=lambda:[[ping(), parse(), outputTiles()]])
+    button2 = ctk.CTkButton(master=window, text="Discover Movies", font=('Merienda', 16), command=lambda:[[ping(), parse(), outputTiles(), hideWidget(cal)]])
+
+    # Function to hide widgets
+    def hideWidget(widget):
+        widget.pack_forget()
 
     # Output
     # outputText = ctk.CTkTextbox(master=window, wrap='word', font=('Merienda', 18), width=600, height=400)
